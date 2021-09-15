50ec51bfbd7eea698f839fe01454b9f254c94cb7
Ryan Driscoll <ryan@reachcreative.com>
2021-09-15 13:59:44 -0600

    fix: using relative import so tests don't break

--- server/src/images/images.service.ts
+++ server/src/images/images.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
-import { ImageEntity } from 'src/entities/Image'
+import { ImageEntity } from '../entities/Image'
 
 @Injectable()
 export class ImagesService {
