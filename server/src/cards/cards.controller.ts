61fc79337bac2ee5e60879ceff36916db43ecb02
RyanDriscoll <driscollrp@gmail.com>
2021-09-15 11:40:44 -0600

    feat(server upload images): updated card update route to store images in disk storage and create image db entries

--- server/src/cards/cards.controller.ts
+++ server/src/cards/cards.controller.ts
@@ -1,12 +1,16 @@
-import { Body, Controller, Logger, Post, Put } from '@nestjs/common'
+import { Body, Controller, Logger, Post, Put, UploadedFiles, UseInterceptors } from '@nestjs/common'
+import { diskStorage } from 'multer'
+import { FilesInterceptor } from '@nestjs/platform-express'
 import { CardEntity } from '../entities/Card'
 import { CardsService } from './cards.service'
+import { editFileName, imageFileFilter } from 'src/utils/image-upload.utils'
+import { ImagesService } from 'src/images/images.service'
 
 @Controller('cards')
 export class CardsController {
   private readonly logger = new Logger(CardsController.name)
 
-  constructor(private cardsService: CardsService) {}
+  constructor(private cardsService: CardsService, private imagesService: ImagesService) {}
 
   @Post()
   addCard(@Body() card: { sectionId: number; title: string }): Promise<CardEntity> {
@@ -16,10 +20,35 @@ export class CardsController {
   }
 
   @Put()
-  updateCard(
-    @Body() card: { id: number; sectionId: number; title: string; description: string }
+  @UseInterceptors(
+    FilesInterceptor('file', 20, {
+      storage: diskStorage({
+        destination: './src/files',
+        filename: editFileName,
+      }),
+      fileFilter: imageFileFilter,
+    })
+  )
+  async updateCard(
+    @Body() card: { id: number; sectionId: number; title: string; description: string },
+    @UploadedFiles() files
   ): Promise<CardEntity> {
     this.logger.log(`PUT /cards/${card.id}`)
+
+    if (files) {
+      await Promise.all(
+        files.map((file) => {
+          this.logger.log(file)
+          const image = {
+            url: `http:/localhost:3001/images/${file.filename}`,
+            name: file.originalname,
+            cardId: card.id,
+          }
+
+          return this.imagesService.create(image)
+        })
+      )
+    }
 
     return this.cardsService.update(card)
   }
