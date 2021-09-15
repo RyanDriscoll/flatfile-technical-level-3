6265a571784476a2f8a59234df48f2a13d8d6bb4
RyanDriscoll <driscollrp@gmail.com>
2021-09-15 11:38:07 -0600

    feat(server upload images): added image service for creating image record

---
+++ server/src/images/images.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+import { ImageEntity } from 'src/entities/Image'
+
+@Injectable()
+export class ImagesService {
+  constructor(
+    @InjectRepository(ImageEntity)
+    private imagesRepository: Repository<ImageEntity>
+  ) {}
+
+  create({
+    cardId,
+    url,
+    name,
+  }: {
+    cardId: number
+    url: string
+    name: string
+  }): Promise<ImageEntity> {
+    let image = new ImageEntity()
+    image.url = url
+    image.name = name
+    image.card_id = cardId
+    return this.imagesRepository.save(image)
+  }
+}
