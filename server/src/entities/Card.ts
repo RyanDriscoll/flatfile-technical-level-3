f23ab15fa8262483311eab4f3020c9099861726a
RyanDriscoll <driscollrp@gmail.com>
2021-09-15 11:42:03 -0600

    added on to many relation for card images

--- server/src/entities/Card.ts
+++ server/src/entities/Card.ts
@@ -1,5 +1,6 @@
-import { Column, Entity, JoinColumn, JoinTable, ManyToOne, PrimaryGeneratedColumn } from 'typeorm'
+import { Column, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm'
 import { SectionEntity } from './Section'
+import { ImageEntity } from './Image'
 
 @Entity({ name: 'cards' })
 export class CardEntity {
@@ -18,4 +19,8 @@ export class CardEntity {
   @ManyToOne(() => SectionEntity, (section) => section.cards)
   @JoinColumn({ name: 'section_id' })
   section: SectionEntity
+
+  @OneToMany(() => ImageEntity, (image) => image.card)
+  @JoinColumn({ referencedColumnName: 'card_id' })
+  images: ImageEntity[]
 }
